42d1b711849f59f2626ca58b45662c9f7f1933c3
maxkoryukov <maxkoryukov@yandex.ru>
2017-03-30 11:56:38 +0500

    Remove dotenv from dependencies
    [skip ci]

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-"use strict";
-
-require('dotenv').config({ silent: true })
+'use strict'
 
 const debug           = require('debug')('volebo:express')
 const fs              = require('fs')
